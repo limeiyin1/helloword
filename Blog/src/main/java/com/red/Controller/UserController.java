4514dad981867ee6cf6fdceb801368a9c41d56ee
limeiyin1 <m17610700898@163.com>
2018-02-13 11:40:53 +0800

    regist

--- Blog/src/main/java/com/red/Controller/UserController.java
+++ Blog/src/main/java/com/red/Controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.red.Pojo.Result;
 import com.red.Pojo.User;
 import com.red.Service.UserService;
 
@@ -17,12 +18,18 @@ public class UserController {
 
 	@RequestMapping("/login")
 	@ResponseBody	
-public void login(User user) {
+public Result regist(User user) {
+		Result  result=null;
 		User user2 = userService.selectByUsername(user.getUsername());
 		if (user2==null) {
 			userService.insertUser(user);
+			result.setStatus(200);
+			result.setMessage("你已注册成功!");
+			return result;
 		}else {
-			
+			result.setStatus(500);
+			result.setMessage("该账号已被注册了，请重新注册！");
+			return  result;
 		}
 			
 	}
